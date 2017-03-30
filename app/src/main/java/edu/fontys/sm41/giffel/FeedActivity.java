53966cc1610c08c7dbd3f7703fbd614f129acd06
Bakker,Tom T.A.C <tac.bakker@student.fontys.nl>
2017-03-30 12:00:55 +0200

    Initial Commit

---
+++ app/src/main/java/edu/fontys/sm41/giffel/FeedActivity.java
@@ -0,0 +1,13 @@
+package edu.fontys.sm41.giffel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
