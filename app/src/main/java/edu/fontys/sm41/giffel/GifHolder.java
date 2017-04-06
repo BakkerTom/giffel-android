c0c650be7ee44a883ffae1a057c9f4499453da0d
Bakker,Tom T.A.C <tac.bakker@student.fontys.nl>
2017-04-06 12:02:41 +0200

    Fixed Picasso loading

--- app/src/main/java/edu/fontys/sm41/giffel/GifHolder.java
+++ app/src/main/java/edu/fontys/sm41/giffel/GifHolder.java
@@ -1,14 +1,20 @@
 package edu.fontys.sm41.giffel;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by tom on 30/03/2017.
@@ -29,11 +35,21 @@ public class GifHolder extends RecyclerView.ViewHolder{
         this.gif = gif;
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference ref = storage.getReferenceFromUrl(gif.getImageUrl());
+        StorageReference ref = storage.getReferenceFromUrl(this.gif.getImageUrl());
 
-        Glide.with(itemView.getContext())
-                .using(new FirebaseImageLoader())
-                .load(ref)
-                .into(gifImageView);
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.with(itemView.getContext())
+                        .load(uri.toString())
+                        .into(gifImageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Storage", "onFailure: Couldn't get download URL" );
+            }
+        });
+
     }
 }
